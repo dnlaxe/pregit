ca43756c5e60a38db5a911b9e0ba747392429647
dnlaxe <dnlaxe@gmail.com>
2026-06-16 22:28:40 +0900

    testing: First test

--- main.go
+++ main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
-	"os/exec"
+
+	_ "github.com/AlecAivazis/survey/v2"
 )
 
 func main() {
-	cmd := exec.Command("git", "status")
 
-	fmt.Println(cmd)
+	out, err := PendingFiles()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	options := parseString(out)
+
+	selected, prefix, message, err := UserInput(options)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	GitProcess(selected, prefix, message)
+
+}
+
+func parseString(input []byte) (files []string) {
+
+	scanner := bufio.NewScanner(bytes.NewReader(input))
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		filename := line[3:]
+		files = append(files, filename)
+	}
+
+	return files
 }
